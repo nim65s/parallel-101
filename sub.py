11cd9efc7acd089542a7a359d37e7cd9ab8b12a2
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-11-10 09:15:40 +0100

    sub: add subprocess example

---
+++ sub.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from subprocess import Popen
+from time import perf_counter
+
+
+def sub(count: int) -> int:
+    start = perf_counter()
+    processes = [Popen(["./task.py"]) for _ in range(count)]
+    for p in processes:
+        p.wait()
+    end = perf_counter()
+
+    return end - start
+
+
+if __name__ == "__main__":
+    print("total:", sub(4))
