d3ab5afbc8e094382dd14c5a501853605dccd4a4
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-11-10 09:36:25 +0100

    add pool example

---
+++ pool.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+
+from concurrent.futures import ProcessPoolExecutor
+from time import perf_counter
+
+from task import task
+
+
+def pool(count: int, size: int):
+    start = perf_counter()
+    with ProcessPoolExecutor() as executor:
+        for t in executor.map(task, [size] * count):
+            print(t)
+    end = perf_counter()
+
+    return end - start
+
+
+if __name__ == "__main__":
+    print("total:", pool(20, 5_000))
